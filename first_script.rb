f8215767294b3974d3a6678a5d938c2d4569139b
Corbett Winningham <corbett.winningham@freshly.com>
2019-11-23 16:06:40 -0700

    adding methods in first_script.rb

--- first_script.rb
+++ first_script.rb
@@ -1,4 +1,40 @@
 require 'watir'
-browser = Watir::Browser.new :chrome
-browser.goto 'http://www.apple.com'
-sleep 5
+
+def go_to_the_puppy_adoption_site
+  @browser = Watir::Browser.new :chrome
+  @browser.goto 'http://puppies.herokuapp.com'
+end
+
+def adopt_puppy_number(num)
+  @browser.button(value: 'View Details', index: num - 1).click
+  @browser.button(value: 'Adopt Me!').click
+end
+
+def continue_adopting_puppies
+  @browser.button(value: 'Adopt Another Puppy').click
+end
+
+def checkout_with(name, address, email, pay_type)
+  @browser.button(value: 'Complete the Adoption').click
+  @browser.text_field(id: 'order_name').set(name)
+  @browser.textarea(id: 'order_address').set(address)
+  @browser.text_field(id: 'order_email').set(email)
+  @browser.select_list(id: 'order_pay_type').select(pay_type)
+  @browser.button(value: 'Place Order').click
+end
+
+def verify_page_contains(text)
+  fail 'Browser text did not match expected value' unless @browser.text.include? text
+end
+
+def wait_to_close(time)
+  sleep time
+end
+
+go_to_the_puppy_adoption_site()
+adopt_puppy_number(1)
+continue_adopting_puppies()
+adopt_puppy_number(2)
+checkout_with('Cheezy', '123 Main St', 'cheezy@foo.com', 'Check')
+verify_page_contains('Thank you for adopting a puppy!')
+wait_to_close(5)
